690caadc0c443fdfe57e33e5d95a273e0ab1d1f3
= <=>
2024-08-04 13:03:51 +0530

    CORS debug 6

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -28,6 +28,11 @@ app.use((0, express_session_1.default)({
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 app.use((0, cors_1.default)());
+app.all("/*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 app.use("/api/v1/auth", Auth_route_1.default);
 app.use("/api/v1/game", Game_route_1.default);
 app.get("/", (req, res) => {
